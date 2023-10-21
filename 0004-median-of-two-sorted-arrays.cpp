8407c1f68b003b1556fb09af47833ed93014980c
Максим <maksim.kurasov.msk@gmail.com>
2023-10-21 22:03:14 +0300

    Median Of Two Sorted Arrays

---
+++ 0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    Given two sorted arrays nums1 and nums2 of size m and n respectively,
+    return the median of the two sorted arrays.
+
+    The overall run time complexity should be O(log (m+n)).
+
+    Input: nums1 = [1,3], nums2 = [2]
+    Output: 2.00000
+
+    Solution:
+
+*/
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(),
+            n2 = nums2.size();
+
+        int l = 0,
+            r = n1 - 1;
+        while(l <= r) {
+            int mid1 = (l + r) / 2;
+            int mid2 = (n2 - mid1)
+
+            if(nums1[mid1] <= nums2[mid2 + 1] && nums2[mid2] <= nums1[mid1 + 1]) {
+                if((n1 + n2) % 2 == 0)
+                    return (max(nums1[mid1], nums2[mid2]) + min(nums1[mid1 + 1], nums2[mid2 + 1])) / 2.0;
+                else
+                    return 0;
+            }
+
+        }
+
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-#include "0033-search-in-rotated-array.cpp"
 
 #define ios ios::sync_with_stdio(0); cin.tie(0);
 #define ll long long
@@ -9,12 +8,26 @@
 using namespace std;
 
 int main() {
-    ios
+    ll k; cin >> k;
+    ll sum = 0;
+    for(int i = 1; i <= k; ++i) {
+        ll a; cin >> a;
+        sum += a;
+        if(i > 2 && sum < 180 * (i - 2)) {
+            cout << -1 << "\n";
+            return 0;
+        }
+    }
 
-    Solution solution;
-    vector<int> input = {1};
+    int minVertices = ceil(sum / 180.0) + 2;
+    cout << minVertices << " ";
 
-    cout << solution.search(input, 0);
+    for(int i = minVertices; i < 10000; ++i) {
+        if((double)(180 * (i - 2) - sum) / (i - k) > 179.0) {
+            cout << i - 1 << "\n";
+            return 0;
+        }
+    }
 
     return 0;
 }
