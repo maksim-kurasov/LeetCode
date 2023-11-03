dd34cd93febb8278ece9ba58bf119582270e8e36
Максим <maksim.kurasov.msk@gmail.com>
2023-11-03 20:57:53 +0300

    Balanced Binary Tree

--- 0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree.cpp
@@ -1,3 +1,46 @@
-//
-// Created by Максим on 03.11.2023.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+    Given a binary tree, determine if it is height-balanced.
+
+    Input: root = [3,9,20,null,null,15,7]
+    Output: true
+
+    Solution:
+    We save the height of each tree and change its condition if
+    the child nodes differ more than 1
+*/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int dfs(TreeNode *root, bool &cond) {
+        if(!root)
+            return 0;
+
+        int left = dfs(root->left, cond);
+        int right = dfs(root->right, cond);
+
+        if(abs(left - right) > 1)
+            cond = false;
+
+        return 1 + max(left, right);
+    }
+
+    bool isBalanced(TreeNode* root) {
+        bool cond = true;
+
+        dfs(root, cond);
+        return cond;
+    }
+};
